c114f283193cf070431ebca2cc54120e3843c400
alex-candido <alex.candido.tec@gmail.com>
2023-03-18 22:51:38 -0300

    implementaçao das classes da camada de modelo

--- src/br/com/treinaweb/semana2/App.java
+++ src/br/com/treinaweb/semana2/App.java
@@ -1,7 +1,15 @@
 package br.com.treinaweb.semana2;
 
+import br.com.treinaweb.semana2.models.Diarista;
+
 public class App {
   public static void main(String[] args) {
-    System.out.println("Olá mundo");
+    var diarista = new Diarista(1L, "Maria da Silva", "(85) 99999-9999", "123.123.123-85", "Av Emilia Gonçalves");
+
+    System.out.println(diarista.getId());
+    System.out.println(diarista.getNome());
+    System.out.println(diarista.getTelefone());
+    System.out.println(diarista.getCpf());
+    System.out.println(diarista.getEndereco());
   }
 }
---
+++ src/br/com/treinaweb/semana2/models/Diarista.java
@@ -0,0 +1,12 @@
+package br.com.treinaweb.semana2.models;
+
+public class Diarista extends Pessoa {
+
+  public Diarista() {
+  }
+
+  public Diarista(Long id, String nome, String telefone, String cpf, String endereco) {
+    super(id, nome, telefone, cpf, endereco);
+  }
+
+}
